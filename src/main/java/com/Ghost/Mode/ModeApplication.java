262a3559bff53f84d84878082d7c1ad11e6276de
Kakade <2400087@cognizant.com>
2026-05-08 12:27:08 +0530

    Initial commit

---
+++ src/main/java/com/Ghost/Mode/ModeApplication.java
@@ -0,0 +1,13 @@
+package com.Ghost.Mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ghost/Mode/ModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ghost.Mode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
